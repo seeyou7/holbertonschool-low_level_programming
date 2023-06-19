00298f59108f5c42f8cfaca5715e014014ee9b07
seeyou7 <>
2023-06-19 07:39:43 -0700

    task -5 print number from 1 to 10

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase and uppercase after.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int x;
+for(x = 0; x <= 9; x++)
+{
+printf("%d", x);
+}
+printf("\n");
+return (0);
+}
