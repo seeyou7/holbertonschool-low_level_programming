afc47a6235ed0749c2e7c3825fb8cb275f360006
seeyou7 <>
2023-06-20 11:59:48 -0700

    task -6 function that computes the absolute value of an integer

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*_abs - function that computes the absolute value of an integer
+*@n: parameter of function
+*
+*Return: return integer
+*/
+
+int _abs(int n)
+{
+
+if (n > 0)
+return (n);
+else if (n == 0)
+return (0);
+else
+return (n * (-1));
+}
