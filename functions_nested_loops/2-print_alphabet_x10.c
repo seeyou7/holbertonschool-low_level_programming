4e1463e74e386e4b8f967e226a790981e4ff1eb4
seeyou7 <>
2023-06-20 07:15:12 -0700

    print 10time the alpha in lower

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* print_alphabet_x10 - truc
+* Description: prints 10 times alphabet
+* Return: void
+*/
+
+void print_alphabet_x10(void)
+{
+
+int i = 0;
+char alpha;
+
+while (i <= 10)
+{
+for (alpha = 'a'; alpha <= 'z'; alpha++)
+{
+_putchar (alpha);
+}
+_putchar ('\n');
+i++;
+}
+}
