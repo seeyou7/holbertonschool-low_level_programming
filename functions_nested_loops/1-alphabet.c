42c25eba8323736d27d90b3c4cb978f4c6ee59f0
seeyou7 <>
2023-06-20 06:41:05 -0700

    task -1 print alphabet

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -6,8 +6,7 @@
 *Return: Always 0
 */
 
-int main(void)
-
+void print_alphabet(void)
 {
 char alpha;
 
@@ -16,5 +15,4 @@ for (alpha = 'a'; alpha <= 'z'; alpha++)
 _putchar (alpha);
 }
 _putchar ('\n');
-return (0);
 }
