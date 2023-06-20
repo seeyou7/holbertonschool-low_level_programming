3c54ccaa9ebe0ad299760b89423c8023a11b61f9
seeyou7 <>
2023-06-20 01:27:30 -0700

    prints all the numbers of base 16 in lowercase using putchar 3time \n

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -8,10 +8,13 @@
 
 int main(void)
 {
-int num;
+char num;
+char alpha;
 
-for (num = 0; num >= 16; num++)
+for (num = '0'; num >= '9'; num++)
 putchar (num);
+for (alpha = 'a'; alpha >= 'f'; alpha++)
+putchar (alpha);
 putchar ('\n');
 return (0);
 }
