7c824fd5991cfd718b2695351cecddee4fa4951b
seeyou7 <>
2023-06-21 13:01:18 -0700

    task -10 function that retur the result of adding 2int

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - function to add two int
+ * @x: the first int
+ * @y: the second int to add
+ * Return: the additional resulat of the two int
+ */
+
+int add(int x, int y)
+{
+	int R;
+
+	R = x + y;
+		return (R);
+}
