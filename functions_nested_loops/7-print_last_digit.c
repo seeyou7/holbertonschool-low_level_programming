2498d8f5b9c860f4732d309dcaff23629866e581
seeyou7 <>
2023-06-21 05:22:59 -0700

    task -7 print last digit with absin case is negative

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+*int print_last_digit function that print last digit of a number
+*@n: parameter of function
+*
+*Return: return value of the last digit
+*/
+
+int print_last_digit(int n)
+{
+
+n = n % 10;
+if (n < 0)
+{
+n = n *(-1);
+_putchar (n + '0');
+return (n);
+}
+_putchar (n+ '0');
+return (n);
+}
+
+
