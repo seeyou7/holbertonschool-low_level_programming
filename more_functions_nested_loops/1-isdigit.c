453e5ae1997f306a2dd056f863b830b191a9d277
seeyou7 <>
2023-06-23 06:28:45 -0700

    task -1 check if c is a digit 1 if true 0 if fals

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *_isdigit - a function that checks for digit from 0 to 9
+ *@c: parameter of the function
+ *
+ *Return: 1 if c is digit returns 0 otherwise
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+	return (0);
+	}
+	else
+	{
+		return (1);
+	}
+}
+
