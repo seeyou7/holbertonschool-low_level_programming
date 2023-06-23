8943b6893cc4eed21ad245904c60c917d5c5c634
seeyou7 <>
2023-06-23 07:30:36 -0700

    task -2 function that multiple a and b

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *mul - function that multiple a by b
+ *@a: parameter of the function
+ *@b:  parameter of the function
+ *Return: Always (0)
+ */
+
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = (a * b);
+	return (mul);
+}
