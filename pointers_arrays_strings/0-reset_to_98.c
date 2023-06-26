a981ca6659ac509c8827a8b32529ccddcd3beca0
seeyou7 <>
2023-06-26 05:01:32 -0700

    task -0

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function that take a pointer to an int
+ *@n: parametre of the function
+ * Return: Always 0.
+ */
+
+void reset_to_98(int *n)
+{
+	 *n = 98;
+}
