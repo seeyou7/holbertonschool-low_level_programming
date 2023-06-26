20c9d29accfcf8dcb7d127f8701f2afefad1ba3c
seeyou7 <>
2023-06-26 06:30:13 -0700

    task -2 function that return strlen

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,18 +1,20 @@
 #include "main.h"
-  
+
 /**
- * _strlen - fucnction that return the lenth of a string 
- *@s: parameter of the function
- * Return: (strlen)
+* _strlen - fucnction that return the lenth of a string
+*@s: parameter of the function
+* Return: (strlen)
  */
 
 int _strlen(char *s)
 {
-  int i = 0;
-  while (*s != '\0')
-    {
-      i++;
-      s++;
-    }
-  return (i);
+	int i = 0;
+
+	while (*s != '\0')
+	{
+		i++;
+		s++
+	}
+	return (i);
+
 }
