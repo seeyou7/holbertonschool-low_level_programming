5609e1fe9298c41bbc7d52c21a016e62a0c8a94a
seeyou7 <>
2023-06-27 05:42:15 -0700

    task -6 that prints every other character of a string, starting with the first character, followed by a new line

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-*_puts2 - function that print every other character
+*puts2 - function that print every other character
 *of a string starting with the first character + \n output is 02468
 *instead of 123456789
 *@str: parameter of the function
