35018de98c3eb90a33a332c20e544a55f3dc37bd
seeyou7 <>
2023-06-27 05:40:05 -0700

    task -6 that prints every other character of a string, starting with the first character, followed by a new line

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-*_puts - function that print every other character
+*_puts2 - function that print every other character
 *of a string starting with the first character + \n output is 02468
 *instead of 123456789
 *@str: parameter of the function
@@ -11,10 +11,21 @@
 
 void puts2(char *str)
 {
-	while (*str != '\0')
+	int len = 0, i;/* len pr stocker la longueur de la chaîne 'str' */
+	/*et i comme IndicePourParcourir la chaîn*/
+
+	while (str[len] != '\0')
 	{
-		_putchar (*str);
-		str += 2;
+		len++;
+	}
+	for (i = 0; i < len; i++)
+		/*boucle est utilisée pour parcourir les caractères de la chaîne.*/
+	{
+		_putchar (str[i]);
+		i += 1;
 	}
 	_putchar ('\n');
 }
+/*En résumé, la fonction puts2 parcourt les caractères de la chaîne str */
+/*en sautant un caractère entre chaque affichage, et affiche ensuite */
+/*les caractères alternatifs suivis d'un saut de ligne*/
