a874dab70dfaa7618c5442634fd2ad0ad8212e8e
seeyou7 <>
2023-06-30 05:24:09 -0700

    task -2 P-3 Function that searches for a character in a string

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strchr - Function that shearch a charcter in string
+ * @s: the string to search in
+ * @c: the charcter to search
+ * Return: a sting that begin with a
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	return (0);
+}
+
