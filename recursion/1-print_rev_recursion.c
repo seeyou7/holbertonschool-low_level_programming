117f4c91dff555123113334d210fab670197cd88
seeyou7 <>
2023-07-03 05:59:42 -0700

    task -1 function that prints a string in reverse

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -9,10 +9,13 @@
 
 void _print_rev_recursion(char *s)
 {
-	while (*s == '\0')
+	if (*s == '\0')
+/* Base case: check if the current character is the null character ('\0')*/
 	{
 		return;
+/*If true, end of string reached, return and terminate the function */
 	}
+/* Recursive case: call the function recursively with the next character in the string*/
 	_print_rev_recursion(s + 1);
 	_putchar(*s);
 }
