dafb862b688490235a589a9a7be5af9e5fa14095
seeyou7 <>
2023-07-05 13:59:25 -0700

    task -3 program that multiplies two numbers

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -22,7 +23,7 @@ int main(int argc, char *argv[])
 	}
 	else
 	{
-		printf("error\n");
+		printf("Error\n");
 		return (1);
 	}
 	return (0);
