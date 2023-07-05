f140293ae33f8b5a5593152ef6bdd29cc8883a03
seeyou7 <>
2023-07-05 12:48:53 -0700

    task -2 program that prints all arguments it receives.

---
+++ argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ * @argc: arg count
+ * @argv: arg vector
+ *
+ * Return: int
+*/
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+	printf("%s\n", argv[i]);
+	}
+	return (0);
+}
