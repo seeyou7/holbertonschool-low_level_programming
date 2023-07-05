6f2522ed9b088b7597e816267a25468ee13e4a7c
seeyou7 <>
2023-07-05 13:50:01 -0700

    task -3 program that multiplies two numbers

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - function to multiplay two number argument
+ * @argc: arg count
+ * @argv: arg vector
+ *
+ * Return: int
+*/
+
+int main(int argc, char *argv[])
+{
+	int v1 = 0, v2 = 0;
+
+	if (argc == 3)
+	{
+		v1 = atoi(argv[1]);
+		v2 = atoi(argv[2]);
+		printf("%d\n", v1 * v2);
+/*atoi to coverte the string into an integer to do the multiplication*/
+	}
+	else
+	{
+		printf("error\n");
+		return (1);
+	}
+	return (0);
+}
