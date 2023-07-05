86055a8e503aaaf3d10349975d968e5440cd482b
seeyou7 <>
2023-07-05 06:28:23 -0700

    task -0  program that prints its name followed by a new line

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - function
+ * @argc: arg count
+ * @argv: arg vector
+ *
+ * Return: int
+ */
+
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
