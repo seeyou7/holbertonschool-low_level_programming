4a1ae074b2bd9a1fe9d8de74e22071b9586910ba
seeyou7 <>
2023-07-19 04:18:06 -0700

    task -1 fix the pointer to fun null condition

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -14,7 +14,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	if (array != 0)
+	if (array != 0 && action != 0)
 	{
 		for (i = 0; i < size; i++)
 			action(array[i]);
