742f979c8dcdcac07e9a4fea922aa262acc66ef0
seeyou7 <>
2023-08-02 06:33:21 -0700

    task -0 fix betty errors

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,10 +1,11 @@
-#include "lists.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include "lists.h"
 
 /**
- * print_list - prints all the elements of a list_t list
- * @h: pointer to the head of the list
+ * print_list - Print all the elements of a list_t list
+ *
+ * @h: pointer to head
  *
  * Return: the number of nodes
  */
@@ -16,10 +17,10 @@ size_t print_list(const list_t *h)
 	for (i = 0; h != NULL; i++)
 	{
 		if (h->str == NULL)
-			printf("[0] (nil)\n");
+		printf("[0] (nil)\n");
 		else
 			printf("[%d] %s\n", h->len, h->str);
-			h = h->next;
+		h = h->next;
 	}
 	return (i);
 }
