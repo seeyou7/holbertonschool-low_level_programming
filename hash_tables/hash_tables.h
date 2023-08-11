f26a2fce119189877921125bc780a4291db4e086
seeyou7 <>
2023-08-11 08:13:24 -0700

    hash function implementing the djb2 algorithm

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -39,5 +39,8 @@ typedef struct hash_table_s
 /* Function prototypes */
 hash_table_t *hash_table_create(unsigned long int size);
 
+unsigned long int hash_djb2(const unsigned char *str);
+
+
 
 #endif /* HASH_TABLES_H */
