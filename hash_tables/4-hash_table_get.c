23882aa735ba9776791d13b4408c2b905609a254
seeyou7 <>
2023-08-11 09:05:52 -0700

    fix betty function that retrieves a value associated with a key

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -26,10 +26,11 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 
 	while (node != NULL)
 	{
-	if (strcmp(node->key, key) == 0)
-
+		if (strcmp(node->key, key) == 0)
 			return (node->value);
-	node = node->next;
+
+		node = node->next;
 	}
+
 	return (NULL);
 }
